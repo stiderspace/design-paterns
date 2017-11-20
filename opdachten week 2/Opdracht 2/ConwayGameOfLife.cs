7c8ea488e8d8abaf320309e159e4844788fd2593
stiderspace <stiderspace@gmail.com>
2017-11-20 23:52:52 +0100

    opdracht 3

--- opdachten week 2/Opdracht 2/ConwayGameOfLife.cs
+++ opdachten week 2/Opdracht 2/ConwayGameOfLife.cs
@@ -3,11 +3,11 @@ using System.Drawing;
 
 namespace ConwayGameOfLife
 {
-  public class ConwayGameOfLife
+  public abstract class ConwayGameOfLife
   {
     const int WIDTH = 300;
     const int HEIGHT = 140;
-    const int LIVECHANCE = 20; // each cell has a change to be (initially) alive
+    const int LIVECHANCE = 10; // each cell has a change to be (initially) alive
 
     private bool[,] space;
     SolidBrush liveBrush = new SolidBrush(Color.Red);
@@ -91,19 +91,7 @@ namespace ConwayGameOfLife
       }
     }
 
-    private bool CellShouldLive(bool livingCell, int neighbourCount)
-    {
-      // with 3 neighbour cell will live
-      if (neighbourCount == 3)
-        return true;
-
-      // live cell with 2 neighbours stays alive
-      if (livingCell && neighbourCount == 2)
-        return true;
-
-      // dead cell
-      return false;
-    }
+    public abstract bool CellShouldLive(bool livingCell, int neighbourCount);
 
     /// <summary>Count live neighbours of given cell.</summary>
     /// <param name="space"></param>
