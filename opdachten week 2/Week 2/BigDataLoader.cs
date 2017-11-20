1f7c0d3f1fd998886245aad3740db923d1b89127
stiderspace <stiderspace@gmail.com>
2017-11-20 23:30:29 +0100

    week 2 opdracht 1

---
+++ opdachten week 2/Week 2/BigDataLoader.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    public abstract class BigDataLoader
+    {
+        public void Etl()
+        {
+            Extract();
+            Transform();
+            Load();
+
+        }
+
+        public abstract void Load();
+        public abstract void Transform();
+        public abstract void Extract();
+    }
+}
---
+++ opdachten week 2/Week 2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitterDataLoader twitter = new TwitterDataLoader();
+            CallDataLoader call = new CallDataLoader();
+            SensorDataLoader sensor = new SensorDataLoader();
+
+            call.Etl();
+            Console.WriteLine();
+            twitter.Etl();
+            Console.WriteLine();
+            sensor.Etl();
+            Console.ReadKey();
+        }
+    }
+}
